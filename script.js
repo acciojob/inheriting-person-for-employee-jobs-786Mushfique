2341bd22fc21a6598be0defb4f155d25913a35f5
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-03-24 23:11:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-// complete this js code
-function Person(name, age) {
-	 constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    greet() {
-        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
-    }
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
-function Employee(name, age, jobTitle) {
- constructor(name, age, jobTitle) {
-        super(name, age);
-        this.jobTitle = jobTitle;
-    }
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); 
+    this.jobTitle = jobTitle;
+  }
 
-    jobGreet() {
-        return `Hello, my name is ${this.name}, I am ${this.age} years old and I work as a ${this.jobTitle}.`;
-    }
+  jobGreet() {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  }
 }
 
-const person1 = new person("Alice", 25);
-console.log(person1.greet()); 
+const person = new Person("Alice", 25);
+person.greet();
 
-const emp1 = new Employee("Bob", 30, "Manager");
-console.log(emp1.greet()); 
-console.log(emp1.jobGreet());
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;

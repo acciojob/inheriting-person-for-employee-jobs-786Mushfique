60677a86ed8e132579364461de285eade650325d
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2026-03-24 23:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	 constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    greet() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+}
+
+function Employee(name, age, jobTitle) {
+ constructor(name, age, jobTitle) {
+        super(name, age);
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old and I work as a ${this.jobTitle}.`;
+    }
+}
+
+const person1 = new person("Alice", 25);
+console.log(person1.greet()); 
+
+const emp1 = new Employee("Bob", 30, "Manager");
+console.log(emp1.greet()); 
+console.log(emp1.jobGreet());
 
 // Do not change code below this line
 window.Person = Person;
